b040ab7669a363b48d68d92f16223aad2424a2af
Daniel Cafe <dcafe@unb.br>
2026-04-24 22:27:39 -0300

    added vcs_all Y

--- snps_dl/database.py
+++ snps_dl/database.py
@@ -3,36 +3,55 @@ list_of_files = {
 	{
 		'v5.10':
 		{
-			'SynopsysInstaller_v5.10.run':           '3722548232'
+			'SynopsysInstaller_v5.10.run': '3722548232'
 		}
 	},
 	'scl':
 	{
 		'v2025.03-SP2':
 		{
-			'scl_v2025.03-SP2_common.spf':           '1492259160',
-			'scl_v2025.03-SP2_linux64.spf':          '1276153600'
+			'scl_v2025.03-SP2_common.spf': '1492259160',
+			'scl_v2025.03-SP2_linux64.spf': '1276153600'
 		}
 	},
 	'vcs_all': 
 	{
 		'vX-2025.06-SP2':
 		{
-			'vcs_vX-2025.06-SP2_common.spf': 		 '3369209935',
+			'vcs_vX-2025.06-SP2_common.spf': '3369209935',
 			'vcs_vX-2025.06-SP2_linux64.spf.part00': '3342841095',
 			'vcs_vX-2025.06-SP2_linux64.spf.part01': '3513736439',
 			'vcs_vX-2025.06-SP2_linux64.spf.part02': '4041256279'
+		},
+		'vY-2026.03':
+		{
+			vcs_vY-2026.03_common.spf': '2586156712'
+			vcs_vY-2026.03_linux64.spf.part00': '3213838152',
+			vcs_vY-2026.03_linux64.spf.part01': '3951557657',
+			vcs_vY-2026.03_linux64.spf.part02': '3050943074',
+			vcs_vY-2026.03_linux64.spf.part03: '2210523'
 		}
 	},
 	'verdi':
 	{
 		'vX-2025.06-SP2':
 		{
-			'verdi_vX-2025.06-SP2_common.spf': 		   '1657599380',
+			'verdi_vX-2025.06-SP2_common.spf': '1657599380',
 			'verdi_vX-2025.06-SP2_linux64.spf.part00': '2168833703',
 			'verdi_vX-2025.06-SP2_linux64.spf.part01': '2772288276',
 			'verdi_vX-2025.06-SP2_linux64.spf.part02': '3085777841'
 		}
 	},
-
+	'fc':
+	{
+		'vX-2025.06-SP2a':
+		{
+			'fusioncompiler_vX-2025.06-SP2_common.spf.part00' : '1065530389',
+			'fusioncompiler_vX-2025.06-SP2_common.spf.part01' :  '253480195',
+			'fusioncompiler_vX-2025.06-SP2_common.spf.part02' : '2763023569',
+			'fusioncompiler_vX-2025.06-SP2_common.spf.part03' : '3161003691',
+			'fusioncompiler_vX-2025.06-SP2_linux64.spf.part00': '1808923199',
+			'fusioncompiler_vX-2025.06-SP2_linux64.spf.part01': '2877446802',
+		}
+	}
 }
